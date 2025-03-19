d1db06ebebc569cfe5b29ae57716ecbff368d82d
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-03-19 21:57:18 +0100

    accounted for edge case where location is unavailable

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button.tsx";
-import { RefreshCw } from "lucide-react";
+import { MapPin, RefreshCw } from "lucide-react";
 import { useGeolocation } from "@/hooks/use-geolocation.tsx";
+import { AlertCircle } from "lucide-react"
+
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert"
 import WeatherSkeleton from "@/components/skeleton";
 
 const WeatherDashboard = () => {
@@ -22,7 +29,7 @@ const WeatherDashboard = () => {
         setTimeout(() => setIsRotating(false), 500); // Reset animation after 0.5s
         getLocation(); // Fetch location on refresh
 
-        if(coordinates){
+        if (coordinates) {
             //reload weather data --unsure
         }
     };
@@ -39,9 +46,8 @@ const WeatherDashboard = () => {
                 <h1 className="text-xl font-bold tracking-tight">My Location</h1>
                 <Button onClick={handleRefresh} variant="outline">
                     <RefreshCw
-                        className={`w-5 h-5 transition-transform duration-500 ease-in-out ${
-                            isRotating ? "rotate-180" : "rotate-0"
-                        }`}
+                        className={`w-5 h-5 transition-transform duration-500 ease-in-out ${isRotating ? "rotate-180" : "rotate-0"
+                            }`}
                     />
                 </Button>
             </div>
@@ -51,7 +57,17 @@ const WeatherDashboard = () => {
                 {locationLoading ? (
                     <p>Fetching location...</p>
                 ) : locationError ? (
-                    <p className="text-red-500">{locationError}</p>
+                    <Alert variant="destructive">
+                        <AlertCircle className="h-4 w-4" />
+                        <AlertTitle>Error</AlertTitle>
+                        <AlertDescription className="flex flex-col gap-4">
+                            <p>{locationError}</p>
+                            <Button onClick={getLocation} variant={"outline"} className="w-fit">
+                                <MapPin className="mr-2 h-4 w-4"/>
+                                Enable Location
+                            </Button>
+                        </AlertDescription>
+                    </Alert>
                 ) : coordinates ? (
                     <p>Latitude: {coordinates.latitude}, Longitude: {coordinates.longitude}</p>
                 ) : null}
